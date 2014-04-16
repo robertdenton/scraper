530fc2d5b638db753a92ff89cc7efbe4b483b389
Robert Denton <robert.denton9@gmail.com>
2014-04-15 21:17:23 -0600

    working scraper that outputs to sqlite db, need to set up cron job now

--- scrapeTwo.py
+++ scrapeTwo.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from bs4 import BeautifulSoup
-import urllib2
 import requests
+import dataset
+import time
+
+db = dataset.connect('sqlite:///headlines.db')
 
 cui = "http://www.cuindependent.com"
 cam = "http://www.dailycamera.com"
@@ -9,11 +12,28 @@ post = "http://www.denverpost.com"
 
 def make_soup(url):
 	get = requests.get(url)
-	print get.status_code
+	#print get.status_code
 	content = get.content
 	soup = BeautifulSoup(content)
-	print soup.prettify()
+	#print soup.prettify()
+	if url == cui:
+		cuiDiv=soup.find("div", "post_header")
+		text=cuiDiv.find('a').text
+		link=cuiDiv.find('a').get('href')
+		#print text
+		#print link
+	else:
+		dfmDiv=soup.find('a', 'listingItemTitle')
+		text=dfmDiv.text
+		link=dfmDiv.get('href')
+		#print text
+		#print link
+	#print text + " " + link
+	table=db['heads']
+	table.insert(dict(day=time.strftime("%m/%d/%Y"),site=url,head=text,link=link))
 
 make_soup(cui)
 make_soup(cam)
 make_soup(post)
+	
+
